6eafea0231cdabaca220cba95f3c8328443a9049
[SW7] 김지훈 <lanslot84@gmail.com>
2024-03-01 22:33:01 +0900

    Feat: options test

---
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+
+// eslint-disable-next-line react/prop-types
+export default function Options({ optionType }) {
+  // optionType is "scopps" or "toppings"
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error response
+        console.log(error);
+      });
+  }, [optionType]);
+
+  // TODO: replace `null` with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
---
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import Options from "../Options";
+
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  // find images ($를 끝에 붙일 경우 마지막에 있다라는 의미)
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  // @ts-ignore
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("[Quiz] topping test", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const altText = toppingImages.map((image) => image.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudges topping",
+  ]);
+});
